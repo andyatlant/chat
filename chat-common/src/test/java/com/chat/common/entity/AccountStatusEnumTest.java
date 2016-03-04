a7020fefdd985417e256ca3491bfc986efe8a9dd
andyatlant <andyatlant@gmail.com>
2016-03-04 06:27:24 +0300

    Add Unit test for registration

--- chat-common/src/test/java/com/chat/common/entity/AccountStatusEnumTest.java
+++ chat-common/src/test/java/com/chat/common/entity/AccountStatusEnumTest.java
@@ -1,13 +1,34 @@
 package com.chat.common.entity;
 
+
 import org.junit.Test;
+
 
 import static org.junit.Assert.*;
 
 public class AccountStatusEnumTest {
 
-    @Test
-    public void testGet() throws Exception {
-//TODO Implement test
+    // TODO
+// I Don't understend
+    @Test(expected = IllegalArgumentException.class)
+    public void testGet_IllegalArgumentException() throws Exception {
+        assertEquals("", AccountStatusEnum.get("Hi"));
     }
+
+    @Test
+    public void testGet_NotConfirmed() {
+        assertEquals(AccountStatusEnum.NOT_CONFIRMED, AccountStatusEnum.get("NotConfirmed"));
+    }
+
+    @Test
+    public void testGet_Confirmed() {
+        assertEquals(AccountStatusEnum.CONFIRMED, AccountStatusEnum.get("Confirmed"));
+    }
+
+    @Test
+    public void testGet_Blocked() {
+        assertEquals(AccountStatusEnum.BLOCKED, AccountStatusEnum.get("Blocked"));
+    }
+
+
 }
